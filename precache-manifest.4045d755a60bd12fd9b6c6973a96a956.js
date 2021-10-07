d93eb55abab2778c927ffdbdca8a8ef8143793b2
Saurabh Kumar <sauravgautam884@gmail.com>
2021-10-07 19:21:08 +0530

    Updates

---
+++ precache-manifest.4045d755a60bd12fd9b6c6973a96a956.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6660a963268bfde1a7784839b2fd9b37",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "50fcf3994b3aa77c450c",
+    "url": "/react-weather-app/static/css/main.21c03947.chunk.css"
+  },
+  {
+    "revision": "d661b0d815234a5401ea",
+    "url": "/react-weather-app/static/js/2.44998ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.44998ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50fcf3994b3aa77c450c",
+    "url": "/react-weather-app/static/js/main.6658011f.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  }
+]);
